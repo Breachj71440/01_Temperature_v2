96b4a365ca73bbce9c8af36e9330a96660c9f8c2
Breachj71440 <51727274+Breachj71440@users.noreply.github.com>
2021-03-18 11:52:30 +1300

    Update 10_history_GUI_v4.py

--- 10_history_GUI_v4.py
+++ 10_history_GUI_v4.py
@@ -13,3 +13,10 @@ class Converter:
         self.converter_frame = Frame(width=300, height=300, bg=background_color,
                                      pady=10)
         self.converter_frame.grid()
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    something = Converter()
+    root.mainloop()
