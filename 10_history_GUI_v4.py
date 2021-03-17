b8d6326c6f4414962ff215042ebf314777e6ba36
Breachj71440 <51727274+Breachj71440@users.noreply.github.com>
2021-03-18 11:50:18 +1300

    Most of temperature converter

---
+++ 10_history_GUI_v4.py
@@ -0,0 +1,15 @@
+import random
+
+from tkinter import *
+from functools import partial   # To prevent unwanted windows
+
+
+class Converter:
+    def __init__(self):
+        # formatting variables...
+        background_color = "light blue"
+
+        # Converter Main Screen GUI...
+        self.converter_frame = Frame(width=300, height=300, bg=background_color,
+                                     pady=10)
+        self.converter_frame.grid()
